fe0a165c7d790aba433820cde35d26c024368d11
xiuguangyao <rufangrongke_2015@163.com>
2015-10-22 13:46:36 +0800

    1022--

---
+++ rufangrongke/rufangrongke/Classes/main/Controller/WXZLoginController.h
@@ -0,0 +1,13 @@
+//
+//  WXZLoginController.h
+//  rufangrongke
+//
+//  Created by 儒房融科 on 15/10/22.
+//  Copyright (c) 2015年 王晓植. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXZLoginController : UIViewController
+
+@end
---
+++ rufangrongke/rufangrongke/Classes/main/Controller/WXZLoginController.m
@@ -0,0 +1,37 @@
+//
+//  WXZLoginController.m
+//  rufangrongke
+//
+//  Created by 儒房融科 on 15/10/22.
+//  Copyright (c) 2015年 王晓植. All rights reserved.
+//
+
+#import "WXZLoginController.h"
+
+@interface WXZLoginController ()
+
+@end
+
+@implementation WXZLoginController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
