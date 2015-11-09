9a6dd6f63e941119d390c8c8d0ac7852e39630d2
xiuguangyao <rufangrongke_2015@163.com>
2015-11-09 11:28:06 +0800

    1106 王晓植合并

---
+++ rufangrongke/rufangrongke/Classes/kehu/View/WXZRealEstateListCell.h
@@ -0,0 +1,17 @@
+//
+//  WXZRealEstateListCell.h
+//  rufangrongke
+//
+//  Created by 儒房融科 on 15/11/4.
+//  Copyright (c) 2015年 王晓植. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXZRealEstateListCell : UITableViewCell
+
++ (instancetype)initRealEstateListCell;
+
+- (void)modifyReportInfo:(NSDictionary *)dic;
+
+@end
