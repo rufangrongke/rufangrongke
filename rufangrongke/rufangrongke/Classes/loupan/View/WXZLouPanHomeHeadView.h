13ad84c90c3976defb29ce767b62ee96f127d759
xiuguangyao <rufangrongke_2015@163.com>
2015-11-17 10:34:16 +0800

    ajvhbhaovboa

---
+++ rufangrongke/rufangrongke/Classes/loupan/View/WXZLouPanHomeHeadView.h
@@ -0,0 +1,13 @@
+//
+//  WXZLouPanHomeHeadView.h
+//  rufangrongke
+//
+//  Created by 儒房融科 on 15/11/17.
+//  Copyright © 2015年 王晓植. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXZLouPanHomeHeadView : UIView
+
+@end
---
+++ rufangrongke/rufangrongke/Classes/loupan/View/WXZLouPanHomeHeadView.m
@@ -0,0 +1,18 @@
+//
+//  WXZLouPanHomeHeadView.m
+//  rufangrongke
+//
+//  Created by 儒房融科 on 15/11/17.
+//  Copyright © 2015年 王晓植. All rights reserved.
+//
+
+#import "WXZLouPanHomeHeadView.h"
+
+@interface WXZLouPanHomeHeadView ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@end
+@implementation WXZLouPanHomeHeadView
+
+
+@end
